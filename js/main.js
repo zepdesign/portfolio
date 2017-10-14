631cc089237208dadd8d4e293500f68b93b5754c
Giuseppe Castiglione <Giuseppe_C_@hotmail.co.uk>
2017-10-14 12:01:38 +0100

    Adding projects

--- js/main.js
+++ js/main.js
@@ -19,4 +19,17 @@ $(document).ready(function(){
         $('.msg-status').addClass('msg-status--successful');
     }; // End Message sent confirmation 
 
+
+
+    $(".slideshow > div:gt(0)").hide();
+
+    setInterval(function() { 
+      $('.slideshow > div:first')
+        .fadeOut(750)
+        .next()
+        .fadeIn(750)
+        .end()
+        .appendTo('.slideshow');
+    }, 3000);
+
 });
